b7cb6a46be2e6c9284204d2c57e49b523b21afb1
kex314 <34056336+kex314@users.noreply.github.com>
2020-01-08 20:00:27 +0300

    Update Edge.hpp

--- cpp/include/Edge.hpp
+++ cpp/include/Edge.hpp
@@ -15,59 +15,48 @@
 #include "Vertex.hpp"
 
 namespace dijkstra {
-
 class Vertex;
 
-class Edge
-{
-public:
-  Edge(Vertex* const from, Vertex* const to, float label) : _from(from), _to(to), _label(label) {};
+	class Edge {
+	public:
+	Edge(Vertex* const from, Vertex* const to, float label) : _from(from), _to(to), _label(label) {};
 
-  Vertex* GetFromVertex() const
-  {
-    return _from;
-  };
+		Vertex* GetFromVertex() const  {
+			return _from;
+		};
 
-  Vertex* GetToVertex() const
-  {
-    return _to;
-  };
+		Vertex* GetToVertex() const  {
+			return _to;
+		};
 
-  void SetFromVertex(Vertex* const vertex)
-  {
-    _from = vertex;
-  }
+		void SetFromVertex(Vertex* const vertex)  {
+			_from = vertex;
+		}
 
-  void SetToVertex(Vertex* const vertex)
-  {
-    _to = vertex;
-  }
+		void SetToVertex(Vertex* const vertex)  {
+			_to = vertex;
+		}
 
-  Vertex* GetPartner(Vertex* vertex) const
-  {
-    if(_from == vertex) {
-      return _to;
-    } else {
-      return _from;
-    }
-  }
+		Vertex* GetPartner(Vertex* vertex) const  {
+			if(_from == vertex) {
+				return _to;
+			} else {
+				return _from;
+			}
+		}
 
-  void SetLabel(float label)
-  {
-    _label = label;
-  };
+		void SetLabel(float label)  {
+			_label = label;
+		};
 
-  float GetLabel() const
-  {
-    return _label;
-  };
+		float GetLabel() const  {
+			return _label;
+		};
 
-private:
-  Vertex* _from;
-  Vertex* _to;
-  float _label;
-};
-
+	private:
+	Vertex* _from;
+	Vertex* _to;
+	float _label;
+	};
 }
-
 #endif
